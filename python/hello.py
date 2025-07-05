8cfd24770a7f5a837c1cdf46d4560fa619198007
Badar <badar.official.19@gmail.com>
2025-07-05 18:02:11 -0400

    initial commit

---
+++ python/hello.py
@@ -0,0 +1,1 @@
+print("Hello,WOrld.Welcome to python community.")
---
+++ python/loop.py
@@ -0,0 +1,3 @@
+a = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"My name is {a} and i'm {age} years old.")
